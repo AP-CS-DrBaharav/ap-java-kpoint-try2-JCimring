6ef11d85e230f6a4461f3d2d57e61b8e36d38429
ZachiBaharav <zachi@baharav.org>
2018-11-13 13:10:16 -0800

    Init

---
+++ src/KPointMain.java
@@ -0,0 +1,25 @@
+
+
+// Class name should be the same as File name
+// one class per file
+// public 
+
+public class KPointMain {
+
+    // special method: main()
+    // where the program starts
+    // public or private 
+    // static 
+    // have to have: return type!!  (void)
+    // method name  (main)
+    // list of arguments  (or empty ()  )
+    //   { }
+    public static void main(String[] args){
+        System.out.println("Starting class KPointMain");
+    }
+    
+    // mininmal method
+    void test1(){
+    }
+    
+}
